bb5207b2a5fcea2c721d465b266ae8dfeada6c87
Alisson A. Tres <git@st.art.br>
2022-04-07 16:10:04 -0300

    Base project.

---
+++ src/app/app-navigation.ts
@@ -0,0 +1,66 @@
+export const navigation = [
+  {
+    text: 'Portal',
+    path: '/portal',
+    icon: 'home'
+  },
+  {
+    text: 'Mosaico',
+    //path: '/',
+    icon: 'smalliconslayout'
+  },
+  {
+    text: 'Auditoria',
+    //path: '/',
+    icon: 'inactivefolder'
+  },
+  {
+    text: 'Mídias',
+    //path: '/tasks',
+    icon: 'video',
+    items: [
+      {
+        text: 'Sub 1',
+        //path: '/profile'
+      },
+      {
+        text: 'Sub 2',
+        //path: '/tasks'
+      }
+    ]
+  },
+  {
+    text: 'Mapa',
+    //path: '/tasks',
+    icon: 'map'
+  },
+  {
+    text: 'Relatórios',
+    //path: '/tasks',
+    icon: 'chart'
+  },
+  {
+    text: 'Configurações',
+    //path: '/tasks',
+    icon: 'preferences'
+  },
+  {
+    text: 'Cadastros',
+    //path: '/tasks',
+    icon: 'edit'
+  }
+  /*{
+    text: 'Examples',
+    icon: 'folder',
+    items: [
+      {
+        text: 'Profile',
+        path: '/profile'
+      },
+      {
+        text: 'Tasks',
+        path: '/tasks'
+      }
+    ]
+  }*/
+];
